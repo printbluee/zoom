65028080349732ced91acfddd02aa5fd83a4b8d4
진혜정 <yhdosa0622@gmail.com>
2022-12-30 17:39:58 +0900

    노마드코더 zoom-#0.3 프론트 Setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
-console.log('ㅎㅇㅎㅇ');
+app.set("view engine", "pug"); 
+// Pug로 view engine 설정
+app.set("views", __dirname + "/views");
+// express에 templatedl 어디 있는지 지정
+app.use("/public", express.static(__dirname + "/public"));
+// public url 생성해 유저에게 파일 공유
+app.get("/", (req, res) => res.render("home"));
+// home.pug를 render 해주는 route handler 만들기
 
-app.listen(3000);
+const handleListten = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListten);
