1e426c4d81addda5c6b2bbdcdd2d3a7e28e0ba9d
진혜정 <yhdosa0622@gmail.com>
2023-02-05 23:47:12 +0900

    노마드코더 zoom 2.8~2.11 방이름띄우기/방 사람수 띄우기/admin apanel

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import http from"http";
 import express from 'express';
-import SocketIO from 'socket.io';
-// import WebSocket from "ws";
-
+import { Server } from 'socket.io';
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
@@ -15,7 +14,35 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListten = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true
+    }
+});
+
+instrument(wsServer, {
+    auth: false,
+  });
+
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: {sids, rooms},
+        },
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;    
+}
+
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
 
 wsServer.on("connection", (socket) => {
     socket['nickname'] = 'Anon';
@@ -25,10 +52,14 @@ wsServer.on("connection", (socket) => {
     socket.on('enter_room', (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit('welcome', socket.nickname);
+        socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit('room_change', publicRooms());
     });
     socket.on('disconnecting', () => {
-        socket.rooms.forEach((room) => socket.to(room).emit('bye', socket.nickname));
+        socket.rooms.forEach((room) => socket.to(room).emit('bye', socket.nickname, countRoom(room) -1));
+    });
+    socket.on('disconnect', () => {
+        wsServer.sockets.emit('room_change', publicRooms());
     });
     socket.on('new_message', (msg,room, done) => {
         socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
